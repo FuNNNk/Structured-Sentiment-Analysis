57e3ceb562f27ac7a9748984ecd1fac8c5bf4614
denis <denis.aenasoaei@yahoo.com>
2021-10-24 12:55:30 +0300

    moved backend project to src folder

---
+++ src/Backend/InputDataReceiver.js
@@ -0,0 +1,9 @@
+class InputDataReceiver{
+    #text;
+    #textFile;
+    #JsonFile;
+
+    getText(text);
+    getTextFile(textFile);
+    getJsonFile(JsonFile); // ar putea merge un chain of responsibility si sa verific daca typeof input primit este de tipul celor din paranteza
+}
---
+++ src/Backend/OutputDataSender.js
@@ -0,0 +1,9 @@
+class OutputDataSender{
+    #text;
+    #textFile;
+    #sJsonFile;
+    OutputDataSender();
+    sendText(text);
+    sendTextFile(textFile);
+    sendJsonFile(JsonFile);
+}
