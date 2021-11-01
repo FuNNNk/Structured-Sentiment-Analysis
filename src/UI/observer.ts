e1ad7dfeaf69abe389bb3822be4e3e4a2ff02925
cosminvlupu <48786005+cosminvlupu@users.noreply.github.com>
2021-11-01 10:19:34 +0200

    Added basic tests

--- src/UI/observer.ts
+++ src/UI/observer.ts
@@ -29,3 +29,40 @@ class Observer {
     }
 
 }
+
+
+import { TestBed } from '@angular/core/testing';
+import {expect} from 'chai'
+import { Source } from "./Source"
+
+it('should be singleton', () => {
+    var dp = new dataPrototype();
+    expect(dp.dataSource).to.equal('Semeval');
+  });
+
+  it('should be true', () => {
+    var dp = new dataPrototype();
+    expect(dp.formatedData).to.equal(true);
+  });  
+
+  let dp = new dataPrototype()
+
+  it('should be true', () => {
+    var obs = new Observer('',dp);
+    expect(obs.SubscribeObserver).to.equal(true);
+  });  
+
+  it('should be true', () => {
+    var obs = new Observer('',dp);
+    expect(obs.NotifyObserver).to.equal(true);
+  });  
+
+  it('should be true', () => {
+    var obs = new Observer('',dp);
+    expect(obs.NotifyAllObservers).to.equal(true);
+  });  
+
+  it('should return list of text', () => {
+    var obs = new Observer('',dp);
+    expect(obs.GetListOfTexts).to.equal([]);
+  });  
