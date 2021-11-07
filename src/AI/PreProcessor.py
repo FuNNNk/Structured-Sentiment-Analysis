ee8080871771adb7b1dbb1a0fa8d55d97407ef9c
Camelia7v <62173925+Camelia7v@users.noreply.github.com>
2021-11-07 16:13:23 +0200

    added json file parser

--- src/AI/PreProcessor.py
+++ src/AI/PreProcessor.py
@@ -2,13 +2,14 @@ from Singleton import Singleton
 from nltk.tokenize import word_tokenize
 from nltk.corpus import stopwords
 from nltk.stem import PorterStemmer
+import json
 
 
 # SINGLETON
 class PreProcessor(metaclass=Singleton):
 
-    def __init__(self, text=None):
-        if text != None:
+    def __init__(self, text):
+        if text is not None:
             self.text = self.pre_process_input(text)
         else:
             self.text = None
@@ -44,6 +45,26 @@ class PreProcessor(metaclass=Singleton):
         return stemmed_text
 
 
+def json_file_parser(file):
+    with open(file) as f:
+        load_file = json.load(f)
+    texts_from_file = []
+    for dic in load_file:
+        # print(dic)
+        texts_from_file.append(dic["text"])
+    return texts_from_file
+
+
 if __name__ == "__main__":
-    pp = PreProcessor("This is a TeST?? I'm just playing.")
-    print(pp.text)
+    # pp = PreProcessor("This is a TeST?? I'm just playing.")
+    # print(pp.text)
+
+    file = "Training data/train.json"
+    list_of_texts = json_file_parser(file)
+    # print(list_of_texts)
+
+    processed_texts = []
+    for text in list_of_texts:
+        pp = PreProcessor(text)
+        # print(pp.pre_process_input(text))
+        processed_texts.append(pp.pre_process_input(text))
