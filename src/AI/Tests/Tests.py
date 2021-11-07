b0f87bd6a882b3077a59eb85d7b5c7d466239ce5
Camelia7v <62173925+Camelia7v@users.noreply.github.com>
2021-11-07 16:22:53 +0200

    update Test.py

--- src/AI/Tests/Tests.py
+++ src/AI/Tests/Tests.py
@@ -6,7 +6,7 @@ class TestPreProcessing(unittest.TestCase):
 
     def test_tokenization(self):
         text = "This is a TeST!?"
-        pp = PreProcessor()
+        pp = PreProcessor(None)
         token_list = pp._tokenize(text)
         for token in token_list:
             self.assertIn(token, ['This', 'is', 'a', 'TeST', '!', '?'], "token_list should contain: 'This', 'is', "
@@ -14,7 +14,7 @@ class TestPreProcessing(unittest.TestCase):
 
     def test_lowercase_transformation(self):
         text = ['This', 'is', 'a', 'TeST', '!', '?']
-        pp = PreProcessor()
+        pp = PreProcessor(None)
         lowercase_text = pp._to_lowercase(text)
         for word in lowercase_text:
             self.assertIn(word, ['this', 'is', 'a', 'test', '!', '?'], "lowercase_text should contain: 'this', 'is', "
@@ -22,14 +22,14 @@ class TestPreProcessing(unittest.TestCase):
 
     def test_stop_words_removing(self):
         text = ['this', 'is', 'a', 'test', '!', '?']
-        pp = PreProcessor()
+        pp = PreProcessor(None)
         text_without_sw = pp._remove_stop_words(text)
         for word in text_without_sw:
             self.assertIn(word, ['test', '!', '?'], "text_without_sw should contain: 'test', '!', '?' ")
 
     def test_stemming(self):
         text = ['playing', 'plays', 'played', "!"]
-        pp = PreProcessor()
+        pp = PreProcessor(None)
         stemmed_text = pp._stem_text(text)
         for word in stemmed_text:
             self.assertIn(word, ['play', 'play', 'play', '!'], "stemmed_text should contain: 'play', 'play', 'play', "
