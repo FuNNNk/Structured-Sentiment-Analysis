445ce0d54a031eddef18a3c810a607f76f54ef3d
denis <denis.aenasoaei@yahoo.com>
2021-12-13 02:02:24 +0200

    added functional AI call for predicting

--- src/AI/ImplementedAlgorithm.py
+++ src/AI/ImplementedAlgorithm.py
@@ -1,53 +1,55 @@
-import tensorflow as tf
-from tensorflow.keras.layers import LSTM
+try:
+    from Algorithm import Algorithm
+    import warnings
+    import tensorflow.keras as keras
+    import numpy as np
+    from tensorflow.keras.models import Sequential
+    from tensorflow.keras import layers
+    from tensorflow.keras.preprocessing.text import Tokenizer
+    from tensorflow.keras.preprocessing.sequence import pad_sequences
+    import itertools
+except ImportError as err:
+    raise ImportError("Not all required modules are installed, please install every module from requirements.txt")
 
-from Algorithm import Algorithm
-import tensorflow.keras as keras
-from tensorflow.keras.models import Sequential
-from tensorflow.keras import layers
-from tensorflow.keras import regularizers
-import h5py
+warnings.filterwarnings('ignore')
+
 
 class NeuralNetworksAlg(Algorithm):
 
     def __init__(self, *args, **kwargs):
-        if 'file_path' in kwargs:
-            try:
-                self.model = keras.models.load_model(kwargs['file_path'])
-            except:
-                print("Could not find the desired model at the file destination, initializing new model")
-                self.model = None
-                self.initialise_model()
-        else:
-            if 'n_layers' in kwargs:
-                self.n_layers = kwargs['n_layers']
+        self.sentiment = ['Negative', 'Positive']
+        self.intensity = ['Standard', 'Strong']
+        self.combinations = []
+        self.max_words = 30
+        self.max_len = 80
 
-            else:
-                self.n_layers = 5
-            if 'n_neurons' in kwargs:
-                self.n_neurons = kwargs['n_neurons']
-            else:
-                self.n_neurons = [16,32,64,32,16]
-            if 'lr' in kwargs:
-                self.lr = kwargs['lr']
-            else:
-                self.lr = 0.01
-            self.model = None
-            self.initialise_model()
+        self.tokenizer = Tokenizer(num_words=self.max_words)
+
+        for item in itertools.product(self.sentiment, self.intensity):
+            self.combinations.append(item[0] + '_' + item[1])
+        print(self.combinations)
+        self.model = None
+        self.initialise_model()
+        if 'weights_file_path' in kwargs:
+            try:
+                self.model.load_weights(kwargs['weights_file_path'])
+            except FileNotFoundError as err:
+                print("Could not find the model weights at the file destination, initializing new model")
+                exit(0)
 
     def initialise_model(self):
-        self.model = Sequential()
-        self.model.add(layers.Embedding(128, 40, input_length=256))
-        self.model.add(layers.Conv1D(20, 6, activation='relu', kernel_regularizer=regularizers.l1_l2(l1=2e-3, l2=2e-3),
-                                 bias_regularizer=regularizers.l2(2e-3)))
-        self.model.add(layers.MaxPooling1D(5))
-        self.model.add(layers.Conv1D(20, 6, activation='relu', kernel_regularizer=regularizers.l1_l2(l1=2e-3, l2=2e-3),
-                                 bias_regularizer=regularizers.l2(2e-3)))
-        self.model.add(layers.GlobalMaxPooling1D())
-        #self.model.add(LSTM(128))
-        self.model.add(layers.Dense(5, activation='softmax'))
-        optimizer = keras.optimizers.Adam(self.lr)
-        self.model.compile(optimizer, loss='categorical_crossentropy', metrics=['acc'])
+        model = Sequential()
+        model.add(layers.Embedding(self.max_words, 128, input_length=self.max_len))
+        model.add(layers.Bidirectional(layers.GRU(64, return_sequences=True)))
+        model.add(layers.Bidirectional(layers.GRU(64)))
+        model.add(layers.Dense(128, activation='relu'))
+        model.add(layers.Dropout(0.5))
+        model.add(layers.Dense(64, activation='relu'))
+        model.add(layers.Dense(len(self.combinations), activation='softmax'))
+
+        model.compile(optimizer='rmsprop', loss='categorical_crossentropy', metrics=['accuracy'])
+
+        self.model = model
 
     def train(self, training_data, n_epochs=70, batch_size=32):
         X_train = [sentence for sentence in training_data['text']]
@@ -57,15 +59,17 @@ class NeuralNetworksAlg(Algorithm):
                 './models/model.acc_{acc:.2f}.hdf5',
                 monitor='acc', verbose=1, save_best_only=True, mode='auto'),
         ]
-        self.history = self.model.fit(X_train, y_train, epochs=n_epochs,
-                                  callbacks=callbacks, lr=self.lr, batch_size=batch_size)
+        history = self.model.fit(X_train, y_train, epochs=15, callbacks=callbacks, batch_size=32)
 
     def test(self, test_data):
         loss, acc = self.model.evaluate(test_data, verbose=2)
         print(f"Accuracy on the test data is {100*acc}%")
 
     def predict_single_input(self, text):
-        self.model.predict(text)
+
+        sequence = self.tokenizer.texts_to_sequences([text])
+        test = pad_sequences(sequence, maxlen=self.max_len)
+        return self.combinations[np.around(self.model.predict(test), decimals=0).argmax(axis=1)[0]]
 
     def save_model(self, file_path):
         self.model.save(file_path)
--- src/AI/main.py
+++ src/AI/main.py
@@ -5,19 +5,19 @@ import os
 
 
 """
-Parse input arguments for training/testing/prediction and type of algorithm
+Parse input arguments for prediction and type of algorithm
 """
 
 # Create parser
 parser = argparse.ArgumentParser(description="Input arguments parser")
 
 # Add arguments
-parser.add_argument("-train", "--train",
-                    action="store_true",
-                    help="The argument that indicates the fact that data in the file is for training.")
-parser.add_argument("-test", "--test",
-                    action="store_true",
-                    help="The argument that indicates the fact that data in the file is for testing.")
+# parser.add_argument("-train", "--train",
+#                     action="store_true",
+#                     help="The argument that indicates the fact that data in the file is for training.")
+# parser.add_argument("-test", "--test",
+#                     action="store_true",
+#                     help="The argument that indicates the fact that data in the file is for testing.")
 parser.add_argument("-predict", "--predict",
                     action="store_true",
                     help="The argument that indicates the fact that data in the file is for prediction.")
@@ -28,34 +28,28 @@ parser.add_argument("path",
 parser.add_argument("-NN", "-nn", "--NN",
                     action="store_true",
                     help="Algorithm type for training option.")
-parser.add_argument("-layers", "-l", "--n_layers",
-                    type=int,
-                    help="The number of layers the neural network must have.")
-parser.add_argument("-neurons", "-n", "--n_neurons",
-                    nargs="*",
-                    type=int,
-                    default=[],
-                    help="The number of neurons for each layer that the neural network must have.")
 
 args = parser.parse_args()
 
-options = [args.train, args.test, args.predict]
+options = [args.predict]
 file_path = args.path
 algorithm = [args.NN]
-nr_layers = args.n_layers
-nr_neurons = args.n_neurons
 
 if not os.path.isfile(file_path):
     print("!!! Invalid file path !!!")
     sys.exit()
 else:
     if any(options):
-        if options[0] and algorithm[0] and nr_layers and nr_neurons:
-            print("0")
-        elif options[1]:
-            print("1")
-        elif options[2]:
-            print("2")
+        if options[0] and algorithm[0]:     # Predict with a neural network
+            prediction_system = PredictionSystem('NN')
+
+            with open(file_path, 'r+') as f:
+                text = f.read()
+                prediction = prediction_system.predict_text(text)
+                # write at the top of the file the prediction for the text
+                f.seek(0)
+                f.write(prediction + '\n' + text)
+
         else:
             print("\n Some arguments are missing! Check out and run again... \n ...or enter 'main.py -h' to see the "
                   "full list of arguments you must have.")
@@ -65,6 +59,4 @@ else:
 
 
 # Example call
-# >main.py -train "path/to/training_data_file" -nn -l 3 -n 5 5 5
-# >main.py -test "path/to/testing_data_file"
-# >main.py -predict "path/to/prediction_data_file"
+# [python or path/to/python/executable] main.py -nn -predict path/to/plainText_file
