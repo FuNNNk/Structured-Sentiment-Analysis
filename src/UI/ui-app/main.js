8d5efe5d706c1aa538fadbd1a35b3f3b37cfa0fe
Bogdan Volosincu <bogdan.volosincu@gmail.com>
2021-12-13 10:41:02 +0200

    add param filename in endpoint parser

--- src/UI/ui-app/main.js
+++ src/UI/ui-app/main.js
@@ -2,11 +2,12 @@
 
 $(document).ready(function(){
 
-
+    window.ssa.loader("stop");
 
     $("#ssa-file-upload-btn").click((e)=>{
        let file = $("#ssa-file-upload").prop('files')[0];
-        window.ssa.uploadFile(file);
+        window.ssa.loader("start");
+        window.ssa.uploadFile(file, $("#ssa-file-upload")[0].files[0].name);
        
     });
 
--- src/UI/ui-app/ssa-lib.js
+++ src/UI/ui-app/ssa-lib.js
@@ -1,6 +1,6 @@
 window.ssa = {};
 
-window.ssa.uploadFile = function (file) {
+window.ssa.uploadFile = function (file, filename) {
 
     let fd = new FormData();
     fd.append('sample', file);
@@ -13,7 +13,7 @@ window.ssa.uploadFile = function (file) {
         contentType: false,
         success: function(data){
             console.log("uploaded", data);
-            window.ssa.analise();
+            window.ssa.analise(filename);
             poolingResults();
         },
         error: function(e) {
@@ -23,11 +23,12 @@ window.ssa.uploadFile = function (file) {
 };
 
 
-window.ssa.analise = function (file) {
+window.ssa.analise = function (filename) {
 
     $.ajax({
         url: "http://localhost:3000/parser",
         method: 'get',
+        data: {filename: filename},
         success: function(data){
             console.log("Started sentiment analysis...");
         },
@@ -37,15 +38,38 @@ window.ssa.analise = function (file) {
     });
 };
 
+
+window.ssa.loader = function (state) {
+    if(!state){
+        $("#spinner").hide();
+        return;
+    }
+
+    if(state == "stop"){
+        $("#spinner").hide();
+    }
+
+    if(state == "start"){
+        $("#spinner").show();
+    }
+    
+    
+};
+
 function poolingResults() {
+    const tm = setTimeout(()=>{
+        window.ssa.loader("start");
+    }, 5000);
+    
     $.ajax({
     url: "http://localhost:3000/stats",
     success: function(data){
-        console.log(data); 
 
         const sentimentResults = JSON.stringify(data, null, '\t');
         $("#rezultate").text(sentimentResults);
+        window.ssa.loader("stop");
         poolingResults();
+
     },
     error: function() {
         poolingResults();
