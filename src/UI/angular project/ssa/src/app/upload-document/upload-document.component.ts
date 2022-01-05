3dbbe9387a6ec4b3da27cbf60676ff57ba898d62
cosminvlupu <48786005+cosminvlupu@users.noreply.github.com>
2022-01-05 05:16:16 +0200

    restructured project using tdd
    removed unused or uneccessary components and service and used tdd to  rewrite used functions in the main components

--- src/UI/angular project/ssa/src/app/upload-document/upload-document.component.ts
+++ src/UI/angular project/ssa/src/app/upload-document/upload-document.component.ts
@@ -12,4 +12,37 @@ export class UploadDocumentComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  fileName= ''
+  file:File
+
+  onFileSelected(event) {
+
+    const file:File = event.target.files[0];
+
+    if (file) {
+
+    this.fileName = file.name;
+    this.file = file
+    }
+  
+  return true
+}
+
+sendFile() {
+  console.log(this.file)
+
+  // const formData = new FormData();
+
+  //   formData.append("file", this.file);
+  //   console.log(this.file)
+
+  //   const upload$ = this.http.post("http://localhost:3000/uiconnector", formData);
+
+  //   upload$.subscribe();
+
+  //   return true
+}
+
+
+
 }
