82a22c866bbb985bd852754ad1e71b74f9876d41
FuNNNk <alexgabi98@yahoo.com>
2022-01-08 11:39:24 +0200

    Refactoring + delete unused code

--- src/Backend/modules/ConnectorService.js
+++ src/Backend/modules/ConnectorService.js
@@ -1,10 +1,7 @@
 const aop = require('../aop')
 const fs = require('fs');
 const path = require('path');
-const AiSentimentConnector = require("./AISentimentConnector")
 var idr = require('./InputDataReceiver')
-var ods = require('./OutputDataSender')
-var idp = require('./InputDataProcessing')
 
 function UIConnector (text) {
     let inputDataReceiver = new idr.InputDataReceiver(text);
@@ -17,11 +14,6 @@ function UIConnector (text) {
             var data = inputDataReceiver.getJsonFile();
         else
             var data = inputDataReceiver.getTextFile();
-}
-
-function setAIConnector (text) {
-    let outputDataSender = new ods.OutputDataSender(text);
-    outputDataSender.sendText();
 }
 
 function getAIConnectorStats (file) {
@@ -51,7 +43,6 @@ function printTypeOfData(data){
 
 module.exports = {
     UIConnector,
-    setAIConnector,
     getAIConnectorStats,
     loggingAspect,
     printTypeOfData
--- src/Backend/modules/InputDataReceiver.js
+++ src/Backend/modules/InputDataReceiver.js
@@ -1,16 +1,13 @@
-var InputDataProcessing = require("./InputDataProcessing");
 
 class InputDataReceiver{
     #text;
     #textFile;
     #JsonFile;
-    inputDataProcessing;
 
     constructor(text, textFile=0, JsonFile=0){
            this.#text = text;
            this.#textFile = textFile;
            this.#JsonFile = JsonFile;
-           let inputDataProcessing = new InputDataProcessing.InputDataProcessing(this.#text);
     };
     getText(){
         if(this.#text != null){
